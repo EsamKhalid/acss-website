083a500ef431ad4502f1cec7ec71f2486462f0cd
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2024-11-16 16:37:43 +0000

    dependencies working fine for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 
 import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
+import test from "node:test";
 
 export default function Home() {
   return (
     <>
+      <Link href="test">Click Here</Link>
       <FaGithub />
     </>
   );
