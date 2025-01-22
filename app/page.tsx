976e1b023fcceec25273196fd89fe99a840233b2
Esam Khalid <esam-khalid@outlook.com>
2025-01-22 21:30:34 +0000

    Starting refactoring on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,17 @@ export default function Page() {
     <>
       <NavBar />
       <div className="px-[25px] pt-[15px] overflow-hidden">
-        <h1 className="text-4xl">// Welcome to the official ACSS Website</h1>
+        <h1 className="text-4xl">// Welcome to the Official ACSS Website</h1>
+        <p className="text-xl mt-5">
+          The Aston Computer Science Society focuses on building a community and
+          fostering a sense of belonging for students at Aston University. With
+          the ACSS now having more events related to Cyber Security and Women in
+          Technology, our society will have a new focus on inclusivity. Whether
+          you study Computer Science, Cyber Security or have never coded before,
+          any Aston student is welcome to attend our events and delve into the
+          world of technology with us. The ACSS hosts two events a week, one
+          technical/professional and one social.
+        </p>
         <p className="text-2xl text-justify mx-5 my-3"></p>
 
         <div className="flex justify-center">
